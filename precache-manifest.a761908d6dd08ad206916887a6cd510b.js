bcdf6581e357988b239ebf04401ec87de79e28cd
Xavier Poon <xavierpoon737@gmail.com>
2020-09-16 21:52:55 +1000

    Updates

---
+++ precache-manifest.a761908d6dd08ad206916887a6cd510b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6328a0da94c5f5aab0413e85b64b3387",
+    "url": "/tabs-collage-info/index.html"
+  },
+  {
+    "revision": "c861e777c611e4b463c5",
+    "url": "/tabs-collage-info/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a207bdab1d111cc2a88b",
+    "url": "/tabs-collage-info/static/js/2.7423f2f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tabs-collage-info/static/js/2.7423f2f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c861e777c611e4b463c5",
+    "url": "/tabs-collage-info/static/js/main.5a699167.chunk.js"
+  },
+  {
+    "revision": "0c5ffe3bc3b22c545e20",
+    "url": "/tabs-collage-info/static/js/runtime-main.75996497.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tabs-collage-info/static/media/logo.5d5d9eef.svg"
+  }
+]);
